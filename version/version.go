44c6e480a0fe465ee684da68edd64714dbe9423b
Davide Madrisan <davide.madrisan@gmail.com>
2018-09-16 14:34:37 +0200

    Gofmt the file version/version.go
    Signed-off-by: Davide Madrisan <davide.madrisan@gmail.com>

--- version/version.go
+++ version/version.go
@@ -27,7 +27,7 @@ var (
 	GitCommit string
 
 	// Version contains the version number that is being run at the moment.
-	Version           = "unknown"
+	Version = "unknown"
 
 	// VersionPrerelease contains a pre-release marker for the version.
 	// If this is "" (empty string) then it means that it is a final release.
